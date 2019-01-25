1f6bc9ce0959e355df1d89eedf58b34e663b56d1
Cobee <uniqtop91@gmail.com>
2019-01-25 23:55:31 +0900

    design settings page

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView, AsyncStorage, Alert } from 'react-native';
+import Modal from "react-native-modal";
 
 
 export default class Settings extends Component {
     constructor(props) {
 		super(props);
 		this.state = {
-            
+            // isVisible: false,
         };
     }
 
@@ -15,26 +16,58 @@ export default class Settings extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.flex_1}>
-                    <Text style={{fontSize:24, textAlign:'center', color: 'white',}}>Information</Text>
+                    <Text style={{fontSize:24, textAlign:'center', color: 'white',}}>앱 정보</Text>
                 </View>
                 <View style={styles.flex_2}>
-                    <View>
-                        <Text>[앱 이름]</Text>
-                        <Text>Version: 1.0</Text>
+                    <View style={styles.name}>
+                        {/* <Image /> */}
+                        <Text style={{color:'#fcf8f3', fontSize:19, fontWeight:'400'}}>Movie Picker</Text>
+                        <Text style={{color:'#fcf8f3', fontSize:13}}>Version: 1.0</Text>
+                        <Text style={{color:'#fcf8f3', fontSize:13}}>Cobee Kwon</Text>
+                        <Text style={{color:'#fcf8f3', fontSize:12}}>Copyright 2019</Text>
                     </View>
-                    <View>
-                        <Text>Licenses</Text>
+                    <View 
+                        style={styles.license}
+                        // onPress={()=>this.setState({isVisible:true})}
+                    >
+                        <Text style={{color:'#fcf8f3', fontSize:17}}>Licenses</Text>
+                        <ScrollView 
+                            style={{flex:3, width:'95%', backgroundColor:'yellow'}} 
+                            contentContainerStyle={{alignItems:'center'}}
+                        >
+                            <Text style={{color:'#fcf8f3', fontSize:13}}>all the libraries...</Text>
+                        </ScrollView>
                         {/* 여기엔 button modal로 IMDB 쓴거에 대한 설명과 copyright */}
+                        <View style={{flex:0.3}}>
+                            <Image 
+                                source={require('../images/movieDB.png')} 
+                                resizeMode={'contain'} 
+                                style={{height:90, backgroundColor:'#21e6c1'}} 
+                            />
+                        </View>
+                        
                     </View>
-                    <View>
-                        <Text>Feedback </Text>
+                    <View style={styles.feedback}>
+                        <Text style={{color:'#fcf8f3', fontSize:17}}>Helps & Feedback</Text>
                     </View>
                 </View>
                 <View style={styles.flex_3}>
                     {/* 여긴 앱 로고 아마도... */}
                 </View>
                 
-                <Text style={{ textAlign:'center', color: 'white',}}>Swipe down to close</Text>
+                <Text style={{ textAlign:'center', color: '#fcf8f3',}}>Swipe down to close</Text>
+
+
+                {/* <Modal
+                    isVisible={this.state.isVisible}
+                    onSwipe={() => this.setState({ isVisible: false })}
+                    swipeDirection="right"
+                >
+                    <View>
+                        <Text style={{color:'#fcf8f3', fontSize:18}}>Licenses</Text>
+                        <Text></Text>
+                    </View>
+                </Modal> */}
             </View>
         )
     }
@@ -47,14 +80,34 @@ const styles = StyleSheet.create({
         },
         flex_1: {
             flex: 2,
-            justifyContent: 'flex-end',
-            paddingBottom: 5,
+            justifyContent: 'flex-start',
+            paddingTop: 10,
         },
         flex_2: {
-            flex: 12,
+            flex: 7,
             backgroundColor: 'transparent',
-        },
+            },
+            name: {
+                flex: 1,
+                alignSelf: 'center',
+                alignItems: 'center',
+                marginBottom: 15,
+            },
+            license: {
+                flex: 4,
+                alignSelf: 'center',
+                alignItems: 'center',
+                marginTop: 15,
+                width: '95%',
+                backgroundColor:'red'
+            },
+            feedback: {
+                flex: 2,
+                alignSelf: 'center',
+                marginTop: 15,
+            },
         flex_3: {
             flex: 1,
+            alignSelf: 'center',
         },
 })
